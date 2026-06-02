6fb53ed1bfe49fc418d9c505aea2fca62d4161d3
Chawye Hsu <su+git@chawyehsu.com>
2026-06-03 01:05:09 +0800

    test(acceptance): add acceptance tests for which command
    Signed-off-by: Chawye Hsu <su+git@chawyehsu.com>

--- tests/acceptance/main.rs
+++ tests/acceptance/main.rs
@@ -16,6 +16,7 @@ cfg_if! {
         mod run_shim_directly;
         mod setup;
         mod verbose_errors;
+        mod which;
         mod volta_bypass;
         mod volta_install;
         mod volta_pin;
