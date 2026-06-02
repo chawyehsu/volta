1e500f1bd7ac7b3df6c1a5104b3dde57886cf314
Chawye Hsu <su+git@chawyehsu.com>
2026-06-02 18:32:47 +0800

    test(acceptance): add test for malformed tool hooks
    Signed-off-by: Chawye Hsu <su+git@chawyehsu.com>

--- tests/acceptance/hooks.rs
+++ tests/acceptance/hooks.rs
@@ -220,6 +220,49 @@ fn malformed_publish_hook_without_url_or_bin_errors() {
 }
 
 #[test]
+fn malformed_tool_hook_without_type_errors() {
+    let s = sandbox()
+        .default_hooks(
+            r#"{
+    "node": {
+        "distro": {}
+    }
+}"#,
+        )
+        .build();
+
+    assert_that!(
+        s.volta("install node@1.2.3"),
+        execs()
+            .with_status(ExitCode::ConfigurationError as i32)
+            .with_stderr_contains("[..]Hook configuration includes no hook types.")
+    );
+}
+
+#[test]
+fn malformed_tool_hook_with_multiple_types_errors() {
+    let s = sandbox()
+        .default_hooks(
+            r#"{
+    "node": {
+        "distro": {
+            "prefix": "[server]/hook/default/node/",
+            "template": "[server]/hook/default/node/{{version}}"
+        }
+    }
+}"#,
+        )
+        .build();
+
+    assert_that!(
+        s.volta("install node@1.2.3"),
+        execs()
+            .with_status(ExitCode::ConfigurationError as i32)
+            .with_stderr_contains("[..]Hook configuration includes multiple hook types.")
+    );
+}
+
+#[test]
 fn redirects_download() {
     let s = sandbox()
         .default_hooks(&default_hooks_json())
