4afc923f9b1999a418e417c66198561e217b84ea
Chawye Hsu <su+git@chawyehsu.com>
2026-09-11 01:54:41 +0800

    fix(session): return ExitCode instead of exiting process for tools
    Signed-off-by: Chawye Hsu <su+git@chawyehsu.com>

--- crates/volta-core/src/session.rs
+++ crates/volta-core/src/session.rs
@@ -3,7 +3,6 @@
 //! hook configuration, and the state of the local inventory.
 
 use std::fmt::{self, Display, Formatter};
-use std::process::exit;
 
 use crate::error::{ExitCode, Fallible, VoltaError};
 use crate::event::EventLog;
@@ -177,7 +176,13 @@ impl Session {
 
     pub fn exit_tool(self, code: i32) -> std::process::ExitCode {
         self.publish_to_event_log();
-        exit(code)
+        // NOTE: Due to limitations in the Rust standard library, there is
+        // currently no suitable way to convert a massive Windows system error
+        // code (or negative exit code) into a `ExitCode` with `u8` inner code;
+        // for the time being, we are using `ExitCode::FAILURE`.
+        u8::try_from(code)
+            .map(std::process::ExitCode::from)
+            .unwrap_or(std::process::ExitCode::FAILURE)
     }
 }
 
